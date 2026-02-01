5419bce945ba82ad57ff6e51b7dbc7bbce205026
Matt Fry <mattmacguyver13@outlook.com>
2026-01-31 23:48:45 -0700

    full js

---
+++ ponder/responsive/responsive.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('nav');
+
+btn.addEventListener('click', togglemenu);
+
+function togglemenu() {
+    menu.classList.toggle('hide');
+    btn.classList.toggle('change');
+}
